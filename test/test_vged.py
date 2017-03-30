6c44388643422a4950f765f685197323c57cd82b
Guillaume de Chambrier <chambrierg@gmail.com>
2017-03-30 23:36:46 +0100

    added plot

--- test/test_vged.py
+++ test/test_vged.py
@@ -1,5 +1,19 @@
 from vged.core import VGed
 import matplotlib.pyplot as plt
+#http://geoexamples.blogspot.co.uk/2014/02/3d-terrain-visualization-with-python.html
+
+def plot1(array_part):
+    fig = plt.figure()
+    plt.imshow(array_part,origin='lower')
+    plt.show()
+
+
+
+def plot2(array_part):
+    from mayavi import mlab
+    mlab.figure(size=(640, 800), bgcolor=(0.16, 0.28, 0.46))
+    mlab.surf(array_part, warp_scale=0.2)
+    mlab.show()
 
 def main():
 
@@ -13,11 +27,8 @@ def main():
 
     array_part = vged.get_area(ul=ul,lr=lr)
 
-
-    fig = plt.figure()
-    plt.imshow(array_part,origin='lower')
-    plt.show()
-
+    #plot1(array_part)
+    plot2(array_part)
 
 
 
